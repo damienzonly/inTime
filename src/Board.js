a82d305c90dc47fd4701bf063cc8c10839becc82
damiano <damianomails@gmail.com>
2019-04-22 18:55:05 +0200

    Display single board items

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,74 @@
 import React, { Component } from "react";
-// import { Layout } from "antd";
+import { Row, Col, Input, Breadcrumb, List } from "antd";
 export default class Board extends Component {
     constructor(props) {
         super(props);
-        this.title = this.props.title;
+        this.state = {
+            currentDraft: "",
+            title: this.props.title,
+            note: this.props.board.note
+        };
+        console.log(this.props.board.note);
     }
+
     render() {
         return (
             <>
-                <h3 className="text-dark">{this.title}</h3>
-                <h2 className="text-dark">{this.props.board.note}</h2>
+                <Row>
+                    <Col>
+                        <Breadcrumb separator="/" style={{ marginLeft: "20vw", marginTop: "60px" }}>
+                            <Breadcrumb.Item>Boards</Breadcrumb.Item>
+                            <Breadcrumb.Item>{this.state.title}</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col
+                        span={12}
+                        offset={6}
+                        style={{
+                            height: "10vh"
+                        }}
+                    >
+                        <div>{this.props.board.note}</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col
+                        span={12}
+                        offset={6}
+                        style={{
+                            marginTop: "100px"
+                        }}
+                    >
+                        <div style={{ textAlign: "center" }}>
+                            <Input
+                                allowClear
+                                value={this.state.currentDraft}
+                                size="large"
+                                placeholder="Add item..."
+                                onChange={e => this.setState({ currentDraft: e.target.value })}
+                                onPressEnter={e => {
+                                    e.preventDefault();
+                                    let nextItem = e.target.value;
+                                    this.props.addItemToBoard(nextItem, this.state.title);
+                                    this.setState({
+                                        currentDraft: ""
+                                    });
+                                }}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12} offset={6}>
+                        <List
+                            size="large"
+                            dataSource={this.props.board.items.map(item => item.text)}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Col>
+                </Row>
             </>
         );
     }
